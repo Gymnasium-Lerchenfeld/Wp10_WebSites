6a8b6e18d9a03f5f14a6d4658e06337a0bd2a96b
Emma Müller <emmaberenikemueller@gmail.com>
2024-04-17 13:15:57 +0200

    liste php fertig

--- 2023_2024/podcast/liste.php
+++ 2023_2024/podcast/liste.php
@@ -5,4 +5,16 @@
     <title>Liste in html</title>
 </head>
     <h1> Hallo </h1>
+
+    <?php
+
+        $array = array("foo", "bar", "hello", "world");
+        echo '<ol>';
+        foreach ($array as $value)
+        {
+            echo '<li>'.$value.'</li>';
+        }
+
+        echo '</ol>';
+    ?>
 </html>
