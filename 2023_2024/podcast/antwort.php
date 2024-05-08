3f9a5d0d1b28b181085ee9e781611bd904d074cb
Antonia Ortmann <antonia.ortmann@gyle.eu>
2024-05-08 12:51:13 +0200

    das funktioniert sogar außer css

--- 2023_2024/podcast/antwort.php
+++ 2023_2024/podcast/antwort.php
@@ -1,67 +1,76 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
-    <title>MySQL Abfrage auf Website</title>
-</head>
-<body>
-    <h2>MySQL Abfrage auf Website</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <label for="query">MySQL-Abfrage:</label><br>
-        <textarea name="query" rows="8" cols="80"><?php if(isset($_POST['query'])) echo $_POST['query']; ?></textarea><br><br>
-        <input type="submit" value="Abfrage ausführen">
-    </form>
- 
-    <?php
-    // Datenbankverbindungsinformationen
-    $servername = "localhost"; // oder IP-Adresse des Servers
-    $username = "GyLeInf"; // Ihr MySQL-Benutzername
-    $password = "GyLeInf"; // Ihr MySQL-Passwort
-    $database = "podcast"; // Der Name Ihrer Datenbank
- 
-    // Verbindung herstellen
-    $conn = new mysqli_connect($servername, $username, $password, $database);
- 
-    // Verbindung überprüfen
+        <link rel="stylesheet" href="css/styles.css">
+        <meta charset="utf-8">
+        <title>Fragen</title>
+    </head>
+    <body>
+        <nav>
+            <a href="index.html"><img src="images/PodcastLogo.jpg"></a>
+            <ul>
+                <li><a class="color-change" href="index.html">Startseite</a></li>
+                <li><a class="color-change" href="trueCrime.html">True Crime</a></li>
+                <li><a class="color-change" href="buecher.html">Bücher</a></li>
+                <li><a class="color-change" href="wieErstelltManEinenPodcast.html">Wie erstellt man einen Podcast</a></li>
+                <li><a class="color-change" href="problematikMitPodcast.html">Problematik mit Podcasts</a></li>
+                <li><a class="active" href="antwort.php">Fragen</a>
+            </ul>
+        </nav>
+<section>
+        <p>Welchen Moderator suchen Sie?</p>
+        <form action="antwort.php" method="POST" id="moderator">
+            <label for="nachname">Nachname</label>
+            <input type="text" name="nachname" id="nachname" maxlength="40"><br>
+
+            <button id="btnSearch" class="submitButton" type="submit">Suchen ...</button>
+        </form>
+    </section>
+
+    <section>
+
+Post-Parameter:<br>    
+<?php
+echo '<pre>'; print_r($_POST); echo '</pre>';
+
+if(isset($_POST['nachname'])) {
+        // Connect to database
+        // Run the query
+        // Display a message that it was successful or not
+    $servername = "localhost";
+    $username = "Podcast";
+    $password = "Podcast";
+    $dbname = "podcast";
+    try {
+        $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
-        die("Verbindung fehlgeschlagen: " . $conn->connect_error);
-    }
- 
-    // SQL-Abfrage ausführen, wenn das Formular abgeschickt wurde
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // SQL-Abfrage aus dem Formular lesen
-        $query = $_POST["query"];
- 
-        // SQL-Abfrage ausführen
-        $result = $conn->query($query);
- 
-        // Überprüfen, ob Datensätze vorhanden sind
-        if ($result) {
-            echo "<h3>Ergebnis der Abfrage:</h3>";
-            echo "<table border='1'>
-                <tr>";
-            // Spaltenüberschriften ausgeben
-            while ($fieldinfo = $result->fetch_field()) {
-                echo "<th>" . $fieldinfo->name . "</th>";
-            }
-            echo "</tr>";
-            // Datensätze ausgeben
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                foreach ($row as $value) {
-                    echo "<td>" . $value . "</td>";
-                }
-                echo "</tr>";
-            }
-            echo "</table>";
-        } else {
-            echo "Fehler bei der Abfrage: " . $conn->error;
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    
+    $result = $conn->query("SELECT ID, Vorname, Nachname, Mail FROM Moderatoren WHERE Nachname LIKE '" . $_POST['nachname'] . "'");
+    if ($result->num_rows > 0) {
+        echo '<table id="ergebnis"><tr><th>ID</th><th>Name</th><th>Mail</th></tr>';
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["ID"]. "</td><td>" . $row["Vorname"]. " " . $row["Nachname"] ."</td><td> " .$row["Mail"] ."</td></tr>";
         }
+        echo "</table>";
+    } else {
+        echo "0 results";
     }
- 
-    // Verbindung schließen
+
     $conn->close();
-    ?>
-</body>
+    echo "Connection closed";
+    } catch (Exception $ex) {
+        echo '<p class="warn">' . $ex->getMessage() . '</p>'; 
+    }
+}
+?>
+</section>
+<footer>
+        <p> hier geht es zurück zur
+        <a class="links" href="index.html">Startseite</a>
+        </p>
+        <p>© 2023 by Em & An</p>
+    </footer>
 </html>
- 
